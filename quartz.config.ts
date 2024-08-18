7d8928c26d053d2fbc340dd55c9be639ebbeb02d
ilLancio <daniele9001@gmail.com>
2024-08-18 03:04:19 +0200

    Quartz sync: Aug 18, 2024, 3:04 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Daniele\nLanciotti",
+    pageTitle: "Daniele&nbsp;Lanciotti",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
